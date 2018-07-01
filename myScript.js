5281ad3ae3ec6fd249772caaba3c17ddfa6176d5
bchengs <38574333+bchengs@users.noreply.github.com>
2018-07-01 15:36:23 -0700

    Add files via upload
    Javascript file

---
+++ myScript.js
@@ -0,0 +1,12 @@
+function myFunction() {  //function to test button
+	var office = "MS Skills: Excel, PowerPoint, Word, Access, OneNote";
+	var programLanguages = "Programming Languages: SQL, Stata, Python, Java, C, SQL, HTML, CSS, JavaScript LC3 Assembly Language";
+	var languages = 'Languages = Cantonese(Proficient), English(Fluent)';
+	document.getElementById("hello").innerHTML = office + "<br>" + programLanguages + "<br>" + languages;
+ }
+function myFunction2() {  
+	var email = "Email: bcheng11@ucsc.edu";
+	var phoneNumber = "Phone Number: (408) 614-5938";
+   document.getElementById("hello2").innerHTML = email + "<br>" + phoneNumber + "<br>" ;
+} 
+
